194f82bcda49cab50fefc05eb25c5c97ae722839
LanaLarsen <kuznetsova_svetlana@lenta.ru>
2018-06-16 16:15:54 +0400

    палиндром

---
+++ task1.rb
@@ -0,0 +1,12 @@
+text = ARGV[0].to_s.downcase
+if text.length > 0 && text.length <= 10000
+  symb = Regexp.new("[ ;:'\"-\\/&@#$%^*()_+=,.!?]")
+  text = text.gsub(symb, '')
+  if text == text.reverse
+    puts 'YES'
+  else
+    puts 'NO'
+  end
+else
+  puts "Text Error"
+end
